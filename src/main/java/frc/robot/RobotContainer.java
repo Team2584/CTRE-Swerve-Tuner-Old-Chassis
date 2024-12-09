1f67319863f50bf9a3bb1d5363469c43d076d365
Mahdi <mahdi.alhassani2007@gmail.com>
2024-12-09 08:48:48 -0800

    init

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -22,36 +22,49 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 
 import frc.robot.generated.TunerConstants;
+import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 
 
 public class RobotContainer {
-  private double governor = 0.35; // Added to slow MaxSpeed to 35%. Set to 1 for full speed.
-  private double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12VoltsMps desired top speed
-  private double MaxAngularRate = RotationsPerSecond.of(1.5*Math.PI).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
+    private double governor = 0.35; // Added to slow MaxSpeed to 35%. Set to 1 for full speed.
+    private double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12VoltsMps desired top speed
+    private double MaxAngularRate = RotationsPerSecond.of(1.5*Math.PI).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
 
-  private final CommandXboxController joystick = new CommandXboxController(0);
+    private final CommandXboxController joystick = new CommandXboxController(0);
 
-  private final Telemetry logger = new Telemetry(MaxSpeed);
+    private final Telemetry logger = new Telemetry(MaxSpeed);
 
-  public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
+    public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
 
-  /* Setting up bindings for necessary control of the swerve drive platform */
-  private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
-          .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
-          .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
-  private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
-  private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
-  private final SwerveRequest.RobotCentric forwardStraight = new SwerveRequest.RobotCentric()
-          .withDriveRequestType(DriveRequestType.OpenLoopVoltage);
+    /* Setting up bindings for necessary control of the swerve drive platform */
+    private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
+            .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
+            .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
+    private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
+    private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
+    private final SwerveRequest.RobotCentric forwardStraight = new SwerveRequest.RobotCentric()
+            .withDriveRequestType(DriveRequestType.OpenLoopVoltage);
 
-  /* Path follower */
-  private final SendableChooser<Command> autoChooser;
+    /* Path follower */
+    private final SendableChooser<Command> autoChooser;
 
-   private final Field2d m_field = new Field2d();
+    private final Field2d m_field = new Field2d();
+
+    private ArmSubsystem buildArm() {
+        return new ArmSubsystem();
+    }
+
+    public ArmSubsystem getArm(){
+        return arm;
+    }
+
+
+    private final ArmSubsystem arm = buildArm();
 
   public RobotContainer() {
         autoChooser = AutoBuilder.buildAutoChooser("Tests");
+
         SmartDashboard.putData("Auto Mode", autoChooser);
         SmartDashboard.putData("Field", m_field);
 
