01e3e09119d64b4b195363e7e8803f8674c8af1f
Mahdi <mahdi.alhassani2007@gmail.com>
2024-12-11 10:54:44 -0800

    mm

--- src/main/java/frc/robot/commands/ArmToPos.java
+++ src/main/java/frc/robot/commands/ArmToPos.java
@@ -1,5 +1,15 @@
 package frc.robot.commands;
 
+import com.ctre.phoenix6.StatusCode;
+import com.ctre.phoenix6.configs.FeedbackConfigs;
+import com.ctre.phoenix6.configs.MotionMagicConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
+
+import static edu.wpi.first.units.Units.RotationsPerSecond;
+import static edu.wpi.first.units.Units.RotationsPerSecondPerSecond;
+import static edu.wpi.first.units.Units.Second;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.ArmSubsystem;
 
@@ -17,12 +27,28 @@ public class ArmToPos extends Command {
 
     @Override
     public void initialize(){
-        
+        FeedbackConfigs fdb = armSubsystem.getFlipperConfig().Feedback;
+        fdb.SensorToMechanismRatio = 85.33; // 12.8 rotor rotations per mechanism rotation
+
+        MotionMagicConfigs mm = armSubsystem.getFlipperConfig().MotionMagic;
+        mm.withMotionMagicCruiseVelocity(RotationsPerSecond.of(0.5)) // 5 (mechanism) rotations per second cruise
+        .withMotionMagicAcceleration(RotationsPerSecondPerSecond.of(10)) // Take approximately 0.5 seconds to reach max vel
+        // Take approximately 0.1 seconds to reach max accel 
+        .withMotionMagicJerk(RotationsPerSecondPerSecond.per(Second).of(50));
+
+
+        Slot0Configs slot0 = armSubsystem.getFlipperConfig().Slot0;
+        slot0.kS = 0.25; // Add 0.25 V output to overcome static friction
+        slot0.kV = 0.12; // A velocity target of 1 rps results in 0.12 V output
+        slot0.kA = 0.01; // An acceleration of 1 rps/s requires 0.01 V output
+        slot0.kP = 60; // A position error of 0.2 rotations results in 12 V output
+        slot0.kI = 0; // No output for integrated error
+        slot0.kD = 0.5; // A velocity error of 1 rps results in 0.5 V output
     }
 
     @Override
     public void execute() {
-
+        armSubsystem.getFlipper().setControl(armSubsystem.getMMVCont().withPosition(setpoint));
     }
 
     @Override
--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -1,26 +1,68 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.configs.FeedbackConfigs;
+import com.ctre.phoenix6.configs.MotionMagicConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
+import com.ctre.phoenix6.controls.PositionVoltage;
+
+
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ArmSubsystem extends SubsystemBase {
 
     // Talon Init
-    private final TalonFX m_claw;
+    private final TalonFX m_intake;
     private final TalonFX m_flip;
 
+    private static final TalonFXConfiguration flipperConfig = new TalonFXConfiguration();
+    private static final TalonFXConfiguration intakeConfig = new TalonFXConfiguration();
+
+    private final MotionMagicVoltage m_mmReq;
     
-    public ArmSubsystem(int clawCAN, int flipCAN){
-        m_claw = new TalonFX(clawCAN);
+    public ArmSubsystem(int intakeCAN, int flipCAN){
+        m_intake = new TalonFX(intakeCAN);
         m_flip = new TalonFX(flipCAN);
+
+        m_mmReq = new MotionMagicVoltage(0);
+    }
+
+    public TalonFX getIntake(){
+        return m_intake;
+    }
+
+    public TalonFX getFlipper(){
+        return m_flip;
+    }
+
+    public TalonFXConfiguration getFlipperConfig(){
+        return flipperConfig;
+    }
+
+    public TalonFXConfiguration getIntakeConfig(){
+        return intakeConfig;
+    }
+
+    public MotionMagicVoltage getMMVCont(){
+        return m_mmReq;
     }
 
     public void setClawSpeed(double speed){
-        m_claw.set(speed);
+        m_intake.set(speed);
     }
 
     public void setFlipSpeed(double speed){
         m_flip.set(speed);
+    }
+
+    public void setSpeed(double speed, String clawflip){
+        if (clawflip.toLowerCase().equals("claw")){
+            m_intake.set(speed);
+        } else if (clawflip.toLowerCase().equals("flip")) {
+            m_flip.set(speed);
+        }
     }
 }
