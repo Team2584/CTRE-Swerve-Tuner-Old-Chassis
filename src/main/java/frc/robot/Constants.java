1faa89f6b7236dda164f8e28bf27a63dab4e1d7f
Mahdi <mahdi.alhassani2007@gmail.com>
2025-02-16 13:43:57 -0800

    Add VisionSub Files

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,5 +1,8 @@
 package frc.robot;
 
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+
 public class Constants {
 
   public static final class ElevatorConstants{
@@ -56,5 +59,34 @@ public class Constants {
     public static final int CORAL_MECH_ID = 23;
     public static final int CORAL_SENSOR_ID = 26;
   }
+  public static final class VisionConstants {
+    // AprilTag layout
+  public static AprilTagFieldLayout aprilTagLayout =
+      AprilTagFieldLayout.loadField(AprilTagFields.k2025Reefscape);
 
+  // Camera names, must match names configured
+  public static String camera0Name = "LL_0";
+  public static String camera1Name = "LL_1";
+
+  // Basic filtering thresholds
+  public static double maxAmbiguity = 0.3;
+  public static double maxZError = 0.75;
+
+  // Standard deviation baselines, for 1 meter distance and 1 tag
+  // (Adjusted automatically based on distance and # of tags)
+  public static double linearStdDevBaseline = 0.02; // Meters
+  public static double angularStdDevBaseline = 0.06; // Radians
+
+  // Standard deviation multipliers for each camera
+  // (Adjust to trust some cameras more than others)
+  public static double[] cameraStdDevFactors =
+      new double[] {
+        1.0, // Camera 0
+        1.0 // Camera 1
+      };
+
+    // Multipliers to apply for MegaTag 2 observations
+    public static double linearStdDevMegatag2Factor = 0.5; // More stable than full 3D solve
+    public static double angularStdDevMegatag2Factor = Double.POSITIVE_INFINITY; 
+  }
 }
